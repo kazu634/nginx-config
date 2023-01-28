f7949783ea7b55ed7c85bfb62d1b33f2d734ab04
Kazuhiro MUSASHI <simoom634@yahoo.co.jp>
2023-01-28 19:46:57 +0900

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+CUR=`pwd`
+
+for CONF in `find ${CUR}/sites-available/ -type f` ; do
+  ln -f -s ${CONF} /etc/nginx/sites-enabled/
+done
+
+for CONF in `find ${CUR}/stream-available/ -type f` ; do
+  ln -f -s ${CONF} /etc/nginx/sites-enabled/
+done
+
+for CONF in `find ${CUR}/consul-template.d/conf/ -type f -name "*.conf"` ; do
+  ln -f -s ${CONF} /etc/consul-template.d/conf/
+done
+
+for TMPL in `find ${CUR}/consul-template.d/templates/ -type f -name "*.tmpl"` ; do
+  ln -f -s ${TMPL} /etc/consul-template.d/templates/
+done
+
+exit 0
